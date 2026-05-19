d12f4de02660284b1f2ea07219b6fb9ee3c34bf2
hexiaol1 <89450840+hexiaol1@users.noreply.github.com>
2026-05-19 17:02:19 +0800

    Update 产能修改.py

--- 产能修改.py
+++ 产能修改.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.express as px
+import plotly.graph_objects as go  # <--- 就是漏了这一行，导致报错
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.ensemble import RandomForestRegressor
